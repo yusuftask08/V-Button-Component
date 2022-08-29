94e3d1d304fff2a274e96155a888f90fdbc8f037
yusuftask08 <yusuftaskiran1@egmail.com>
2022-08-29 12:46:11 +0300

    added button component

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="app-view">
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+    <div class="buttons">
+      <Button
+       v-for="button in buttonSm" :key="button.id" 
+       :size="button.size" 
+       :color="button.color" 
+       :innerText="button.innerText" 
+       @onClick="button.onClick()">
+
+       {{button.vSlot}}
+      </Button>
+    </div>
+    <div class="buttons">
+      <Button
+       v-for="button in buttonMd" :key="button.id" 
+       :size="button.size" 
+       :color="button.color" 
+       :innerText="button.innerText" 
+       @onClick="button.onClick()">
+
+       {{button.vSlot}}
+      </Button>
+    </div>
+    <div class="buttons">
+      <Button
+       v-for="button in buttonLg" :key="button.id" 
+       :size="button.size" 
+       :color="button.color" 
+       :innerText="button.innerText" 
+       @onClick="button.onClick()">
+
+       {{button.vSlot}}
+      </Button>
+    </div>
+    <div class="buttons">
+      <Button
+       v-for="button in buttonxLg" :key="button.id" 
+       :size="button.size" 
+       :color="button.color" 
+       :innerText="button.innerText" 
+       @onClick="button.onClick()">
+
+       {{button.vSlot}}
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/Button/index.vue'
+import buttons from '../public/buttons.json'
+
+export default {
+  name: 'App',
+  components: {
+    Button
+  },
+  methods: {
+    btnClick() {
+      alert('butona tıklandı')
+    }
+  },
+  computed:{
+    buttonSm(){
+      return buttons.map((object) => {
+        return {
+          ...object,
+          onClick(){
+            alert('Small butona tıklandı')
+          }
+        }
+      })
+    },
+    buttonMd(){
+      return buttons.map((object) => {
+        return {
+          ...object,
+          size:'medium',
+          onClick(){
+            alert('Medium butona tıklandı')
+          }
+        }
+      })
+    },
+    buttonLg(){
+      return buttons.map((object) => {
+        return {
+          ...object,
+          size:'large',
+          onClick(){
+            alert('Large butona tıklandı')
+          }
+        }
+      })
+    },
+    buttonxLg(){
+      return buttons.map((object) => {
+        return {
+          ...object,
+          size:'xLarge',
+          onClick(){
+            alert('xLarge butona tıklandı')
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Button/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <button class="button" 
+     :class="[color, size, {disabled: disabled}]"
+     :type="type" 
+     @click="$emit('onClick')"
+     :disabled="disabled"
+     style="margin:10px"
+     >
+        <span class="button-text" v-if="innerText"> {{  innerText  }} </span>
+        <span class="button-text" v-else>
+            <slot></slot>
+        </span>
+    </button>
+</template>
+<script>
+export default {
+    name: 'GButton',
+    props: {
+        color: {
+            type: String,
+            default: 'primary'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        size: {
+            type: String,
+            default: 'medium'
+        },
+        innerText: {
+            type: String,
+        },
+        type:{
+            type:String,
+            default:'submit'
+        }
+    }
+}
+</script>
+<style lang='scss'>
+@import '../../style/style.scss';
+</style>
